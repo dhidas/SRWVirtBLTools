a4f486ce9f87d3711126e4df784a3ff714c5ac20
dhidas-bnl-gov <dhidas@bnl.gov>
2016-03-23 09:10:08 -0400

    so I do not have to keep typing it

---
+++ scripts/ConvertKymaData_ESM.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./ConvertKymaTableToSRWFormat.py ~/Desktop/1449-FMAC/UMEA_20151123_gaps16-20_newCCtable/1449-TPF-UMEA_20151123-MRR.txt mm/epu57_esm_sum_1.txt ~/Desktop/1449-FMAC/UMEA_20151123_gaps16-20_newCCtable/datafiles/ mm/
+
+./ConvertKymaTableToSRWFormat.py ~/Desktop/1449-FMAC/UMEA_20151124_gaps21-27_newCCtable/1449-TPF-UMEA_20151124-MRR.txt mm/epu57_esm_sum_2.txt ~/Desktop/1449-FMAC/UMEA_20151124_gaps21-27_newCCtable/datafiles/ mm/
+
+./ConvertKymaTableToSRWFormat.py ~/Desktop/1449-FMAC/UMEA_20151129_gaps28-60_newCCtable/1449-TPF-UMEA_20151129-MRR.txt mm/epu57_esm_sum_3.txt ~/Desktop/1449-FMAC/UMEA_20151129_gaps28-60_newCCtable/datafiles/ mm/
